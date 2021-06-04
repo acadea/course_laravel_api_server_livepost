bec967b1d1d67a4e091cc8147472088dc8d3183b
Sam <opensource@acadea.io>
2021-06-04 18:24:06 +0800

    added routes for users

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -47,6 +47,11 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
+
+
+//        Route::bind('user', function ($value){
+//            return 12345;
+//        });
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,35 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/users', function (Request $request){
+    return new \Illuminate\Http\JsonResponse([
+        'data' => 'aaaa'
+    ]);
+});
+
+Route::get('/users/{user}', function (\App\Models\User $user){
+    return new \Illuminate\Http\JsonResponse([
+        'data' => $user
+    ]);
+});
+
+Route::post('/users', function (){
+    return new \Illuminate\Http\JsonResponse([
+        'data' => 'posted'
+    ]);
+});
+
+Route::patch('/users/{user}', function (\App\Models\User $user){
+    return new \Illuminate\Http\JsonResponse([
+        'data' => 'patched'
+    ]);
+});
+
+Route::delete('/users/{user}', function (\App\Models\User $user){
+    return new \Illuminate\Http\JsonResponse([
+        'data' => 'deleted'
+    ]);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
