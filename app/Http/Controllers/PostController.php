b20710315678bcb74b7ac7360f8d4992cd1ff205
Sam <opensource@acadea.io>
2021-08-20 21:46:57 +0800

    added custom validator in store method of post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,10 +7,12 @@ use App\Http\Requests\PostStoreRequest;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use App\Repositories\PostRepository;
+use App\Rules\IntegerArray;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -33,13 +35,30 @@ class PostController extends Controller
      * @param  PostStoreRequest  $request
      * @return PostResource
      */
-    public function store(PostStoreRequest $request, PostRepository $repository)
+    public function store(Request $request, PostRepository $repository)
     {
-        $created = $repository->create($request->only([
+        $payload = $request->only([
             'title',
             'body',
             'user_ids'
-        ]));
+        ]);
+        Validator::validate($payload, [
+            'title' => 'string|required',
+            'body' => ['string', 'required'],
+            'user_ids' => [
+                'array',
+                'required',
+                new IntegerArray(),
+            ]
+        ], [
+            'body.required' => "Please enter a value for body.",
+            'title.string' => 'HEYYYY use a string',
+        ], [
+            'user_ids' => 'USERR IDDD'
+        ]);
+
+
+        $created = $repository->create($payload);
 
         return new PostResource($created);
     }
