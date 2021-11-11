b224c789dd4542967bd806a7249319eb55c91ac7
Sam <opensource@acadea.io>
2021-11-11 22:23:21 +0800

    fortify password reset and user registration

--- config/fortify.php
+++ config/fortify.php
@@ -134,9 +134,9 @@ return [
     'features' => [
         Features::registration(),
         Features::resetPasswords(),
-        // Features::emailVerification(),
-        Features::updateProfileInformation(),
-        Features::updatePasswords(),
+//        // Features::emailVerification(),
+//        Features::updateProfileInformation(),
+//        Features::updatePasswords(),
 //        Features::twoFactorAuthentication([
 //            'confirmPassword' => true,
 //        ]),
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/reset-password/{token}', function ($token){
+    return view('auth.password-reset', [
+        'token' => $token
+    ]);
+})->middleware(['guest:'.config('fortify.guard')])
+  ->name('password.reset');
+
 if(\Illuminate\Support\Facades\App::environment('local')){
 
     Route::get('/playground', function (){
